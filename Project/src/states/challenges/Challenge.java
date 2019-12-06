b7e74d03e3345f4564026a909b9c774f7382fe38
David Charkey <skywalker_dc@live.com.au>
2019-12-06 08:05:54 +0800

    - Significant progress on inventory state - Added ButtonGrid constructor argument checking - Entities hold images of ground AND themselves - Entity stores are now global - Dev Data setting removed - Music setting added - IDE mode = fast load, no music, dev data, all items, faster run, no popups - Controls changed and expanded - New popup background and new text color option - Cleaned up actual game state by offloading processing - Offloaded input handling to Input Handler + Created keyMap - Item handling offloaded to item store
    Signed-off-by: David Charkey <skywalker_dc@live.com.au>

---
+++ Project/src/states/challenges/Challenge.java
@@ -0,0 +1,45 @@
+package states.challenges;
+
+import states.screens.InfoScreen;
+import main.Globals;
+
+import java.util.ArrayList;
+
+/**
+ * Base class for challenge states
+ *
+ * @author David
+ */
+public class Challenge extends InfoScreen {
+
+    /**
+     * Return ID used to identify state
+     *
+     * @return ID
+     */
+    @Override
+    public int getID() {
+        return Globals.STATES.get("CHALLENGE");
+    }
+
+    /**
+     * Set button labels
+     *
+     * @return
+     */
+    @Override
+    public ArrayList<String> getButtonLabels() {
+        ArrayList<String> lines = new ArrayList<>();
+
+        lines.add("CHALLENGE");
+        lines.add("This is where a boss battle would be!");
+        lines.add("Would be quite complex though...");
+        lines.add("May come in next version! ");
+        lines.add(" ");
+        lines.add("To resume play: Press ESC to go to the MainMenu,");
+        lines.add("and click on CONTINUE");
+
+        return lines;
+    }
+
+}
